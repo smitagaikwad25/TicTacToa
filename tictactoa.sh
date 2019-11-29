551efbcf99c0c3d1146ceb81ea79d2fc74db8416
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-11-29 12:00:03 +0530

    Add : funtion to reset bord

--- tictactoa.sh
+++ tictactoa.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo "welCome In Tictactoa Simulator"
+
+function resetBord()
+{
+        j=0
+        for(( i=1; i<=9; i++ ))
+        do
+                bord[$i]=$j
+        done
+
+}
+
