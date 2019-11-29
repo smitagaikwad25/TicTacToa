6655606513fa10136d86415a1c3001599c81d483
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-11-29 12:10:32 +0530

    Add : funtion to assign X Or O to players

--- tictactoa.sh
+++ tictactoa.sh
@@ -11,3 +11,27 @@ function resetBord()
 
 }
 
+function assignLetterXOrO()
+{
+	assign=1
+	random=$((RANDOM%2))
+	if [ $assign -eq $random ]
+        then
+                player=X
+                computer=O
+                echo "player:" $player 
+                echo "computer:" $computer
+        else
+                player=O
+                computer=X
+                echo "player:" $player 
+                echo "computer:" $computer
+        fi
+
+
+}
+
+assignLetterXOrO
+resetBord
+
+
